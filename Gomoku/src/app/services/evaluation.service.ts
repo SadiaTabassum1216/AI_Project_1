0c5f5ca99482436b4ce29053600651ec636fd8bf
maimunafaria <bsse1222@iit.du.ac.bd>
2023-09-06 21:59:13 +0600

    evaluation changing

--- Gomoku/src/app/services/evaluation.service.ts
+++ Gomoku/src/app/services/evaluation.service.ts
@@ -1,3 +1,118 @@
+
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvaluationService {
+
+  constructor() { }
+
+  // Evaluate the board
+  evaluate(board: string[][]): number {
+    // Define the score values for different game states
+    const winScore = 10000;
+    const loseScore = -10000;
+    const tieScore = 0;
+
+    // Check for a winning state for 'O'
+    if (this.checkWinningState(board, 'O')) {
+      return winScore;
+    }
+
+    // Check for a winning state for 'X'
+    if (this.checkWinningState(board, 'X')) {
+      return loseScore;
+    }
+
+    // If neither 'O' nor 'X' has won, return a tie score
+    return tieScore;
+  }
+
+   // Define the dimensions of the game board
+   boardSize = 10;
+
+   // Helper function to generate all possible horizontal, vertical, and diagonal winning patterns
+   generateWinningPatterns(boardSize: number): number[][][] {
+     const patterns: number[][][] = [];
+ 
+     // Horizontal patterns
+     for (let row = 0; row < boardSize; row++) {
+       for (let col = 0; col <= boardSize - 5; col++) {
+         const pattern: number[][] = [];
+         for (let i = 0; i < 5; i++) {
+           pattern.push([row, col + i]);
+         }
+         patterns.push(pattern);
+         console.log(patterns);
+       }
+     }
+ 
+     // Vertical patterns
+     for (let row = 0; row <= boardSize - 5; row++) {
+       for (let col = 0; col < boardSize; col++) {
+         const pattern: number[][] = [];
+         for (let i = 0; i < 5; i++) {
+           pattern.push([row + i, col]);
+         }
+         patterns.push(pattern);
+        
+       }
+     }
+ 
+     // Diagonal patterns (top-left to bottom-right)
+     for (let row = 0; row <= boardSize - 5; row++) {
+       for (let col = 0; col <= boardSize - 5; col++) {
+         const pattern: number[][] = [];
+         for (let i = 0; i < 5; i++) {
+           pattern.push([row + i, col + i]);
+         }
+         patterns.push(pattern);
+         
+       }
+     }
+ 
+     // Diagonal patterns (top-right to bottom-left)
+     for (let row = 0; row <= boardSize - 5; row++) {
+       for (let col = boardSize - 1; col >= 4; col--) {
+         const pattern: number[][] = [];
+         for (let i = 0; i < 5; i++) {
+           pattern.push([row + i, col - i]);
+         }
+         patterns.push(pattern);
+        
+       }
+     }
+ 
+     return patterns;
+   }
+ 
+   // Combine all winning patterns into one array
+   allWinningPatterns: number[][][] = this.generateWinningPatterns(this.boardSize);
+ 
+   // Helper function to check if a player has won
+   checkWinningState(board: string[][], player: string): boolean {
+     for (const pattern of this.allWinningPatterns) {
+       if (this.checkPattern(board, pattern, player)) {
+         return true;
+       }
+     }
+     return false;
+   }
+ 
+   // Helper function to check if a specific pattern is present on the board
+   checkPattern(board: string[][], pattern: number[][], player: string): boolean {
+     for (const [row, col] of pattern) {
+       if (board[row][col] !== player) {
+         return false;
+       }
+     }
+     return true;
+   }
+
+}
+
 // import { Injectable } from '@angular/core';
 
 // @Injectable({
@@ -206,133 +321,3 @@
 
  
 // }
-
-
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class EvaluationService {
-
-  constructor() { }
-
-  // Evaluate the board
-  evaluate(board: string[][]): number {
-    // Define the score values for different game states
-    const winScore = 10000;
-    const loseScore = -10000;
-    const tieScore = 0;
-
-    // Check for a winning state for 'O'
-    if (this.checkWinningState(board, 'O')) {
-      return winScore;
-    }
-
-    // Check for a winning state for 'X'
-    if (this.checkWinningState(board, 'X')) {
-      return loseScore;
-    }
-
-    // If neither 'O' nor 'X' has won, return a tie score
-    return tieScore;
-  }
-
-<<<<<<< HEAD:Gomoku/src/app/services/pruning.service.ts
-   // Define the dimensions of the game board
-   boardSize = 10;
-=======
-// Define scoring patterns and their corresponding scores
-//  scoringPatterns: { [pattern: string]: number } = {
-//   '11110': 1000,  // Five Xs in a row with one empty end
-//   '011110': 500, // Four Xs in a row with one empty end
-//   '01110': 50,   // Four Xs in a row with no empty end
-//   '001110': 25,  // Three Xs in a row with one empty end
-//   '011100': 25,  // Three Xs in a row with one empty end
-//   '0001110': 5,  // Two Xs in a row with one empty end
-//   '011000': 5,   // Two Xs in a row with one empty end
-//   '001100': 2,   // Two Xs in a row with no empty end
-//   '000110': 2,   // Two Xs in a row with no empty end
-// };
->>>>>>> ea7f61af78b6dc653ebddfc5c4b1035ba2ca9d95:Gomoku/src/app/services/evaluation.service.ts
-
-   // Helper function to generate all possible horizontal, vertical, and diagonal winning patterns
-   generateWinningPatterns(boardSize: number): number[][][] {
-     const patterns: number[][][] = [];
- 
-     // Horizontal patterns
-     for (let row = 0; row < boardSize; row++) {
-       for (let col = 0; col <= boardSize - 5; col++) {
-         const pattern: number[][] = [];
-         for (let i = 0; i < 5; i++) {
-           pattern.push([row, col + i]);
-         }
-         patterns.push(pattern);
-         console.log(patterns);
-       }
-     }
- 
-     // Vertical patterns
-     for (let row = 0; row <= boardSize - 5; row++) {
-       for (let col = 0; col < boardSize; col++) {
-         const pattern: number[][] = [];
-         for (let i = 0; i < 5; i++) {
-           pattern.push([row + i, col]);
-         }
-         patterns.push(pattern);
-        
-       }
-     }
- 
-     // Diagonal patterns (top-left to bottom-right)
-     for (let row = 0; row <= boardSize - 5; row++) {
-       for (let col = 0; col <= boardSize - 5; col++) {
-         const pattern: number[][] = [];
-         for (let i = 0; i < 5; i++) {
-           pattern.push([row + i, col + i]);
-         }
-         patterns.push(pattern);
-         
-       }
-     }
- 
-     // Diagonal patterns (top-right to bottom-left)
-     for (let row = 0; row <= boardSize - 5; row++) {
-       for (let col = boardSize - 1; col >= 4; col--) {
-         const pattern: number[][] = [];
-         for (let i = 0; i < 5; i++) {
-           pattern.push([row + i, col - i]);
-         }
-         patterns.push(pattern);
-        
-       }
-     }
- 
-     return patterns;
-   }
- 
-   // Combine all winning patterns into one array
-   allWinningPatterns: number[][][] = this.generateWinningPatterns(this.boardSize);
- 
-   // Helper function to check if a player has won
-   checkWinningState(board: string[][], player: string): boolean {
-     for (const pattern of this.allWinningPatterns) {
-       if (this.checkPattern(board, pattern, player)) {
-         return true;
-       }
-     }
-     return false;
-   }
- 
-   // Helper function to check if a specific pattern is present on the board
-   checkPattern(board: string[][], pattern: number[][], player: string): boolean {
-     for (const [row, col] of pattern) {
-       if (board[row][col] !== player) {
-         return false;
-       }
-     }
-     return true;
-   }
-
-}
-
