829a80932bffe78518f2e6ba5532a3884b43e07b
maimunafaria <bsse1222@iit.du.ac.bd>
2023-09-09 22:45:47 +0600

    board updated

--- Gomoku/src/app/services/minimax.service.ts
+++ Gomoku/src/app/services/minimax.service.ts
@@ -1,5 +1,70 @@
+// import { Injectable } from '@angular/core';
+// import { EvaluationService } from './evaluation.service';
+// import { MovesService } from './moves.service';
+// import { CheckBoardService } from './check-board.service';
+// import { Evaluation2Service } from './evaluation_new.service';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MinimaxService {
+
+//   constructor(
+//     private evaluation: EvaluationService,
+//     private moves: MovesService,
+//     private check: CheckBoardService) { }
+
+//   maxDepth: number = 3;
+
+//   calculateComputerMove(board: string[][]): [number, number] | null {
+//     const bestMove = this.minimax(board, this.maxDepth, true, -Infinity, Infinity);
+//     console.log("Best Move score: " + bestMove.score);
+//     console.log("Best Move: " + bestMove.move);
+//     return bestMove.move;
+//   }
+
+//   minimax(board: string[][], depth: number, maximizingPlayer: boolean, alpha: number, beta: number): 
+//   { score: number; move: [number, number] } {
+//   const result = this.check.checkGameStatus(board);
+
+//   if (result !== null || depth <= 0) {
+//     return { score: this.evaluation.evaluateRelativeScoreForComputer(board, maximizingPlayer), move: [-1, -1] };
+//     // return { score: this.evaluation_2.evaluate(board, maximizingPlayer), move: [-1, -1] };
+//   }
+
+//   let bestMove: [number, number] = [-1, -1];
+//   let bestScore: number = maximizingPlayer ? -Infinity : Infinity;
+
+//   const validMoves: [number, number][] = this.moves.generateMoves(board);
+
+//   for (const [row, col] of validMoves) {
+//     board[row][col] = maximizingPlayer ? 'O' : 'X'; // Make a move
+//     const score = this.minimax(board, depth - 1, !maximizingPlayer, alpha, beta).score; 
+//     board[row][col] = ''; // Undo the move
+
+//     if (maximizingPlayer && score > bestScore) {
+//       bestScore = score;
+//       bestMove = [row, col];
+//       alpha = Math.max(alpha, bestScore);
+//     } else if (!maximizingPlayer && score < bestScore) {
+//       bestScore = score;
+//       bestMove = [row, col];
+//       beta = Math.min(beta, bestScore);
+//     }
+
+//     if (alpha >= beta) {
+//       break; // Prune the branch
+//     }
+//   }
+
+//   return { score: bestScore, move: bestMove };
+// }
+
+  
+// }
+
 import { Injectable } from '@angular/core';
-import { EvaluationService } from './evaluation.service';
+import { EvaluationJavaService } from './evaluation-java.service';
 import { MovesService } from './moves.service';
 import { CheckBoardService } from './check-board.service';
 import { Evaluation2Service } from './evaluation_new.service';
@@ -10,20 +75,20 @@ import { Evaluation2Service } from './evaluation_new.service';
 export class MinimaxService {
 
   constructor(
-    private evaluation: EvaluationService,
+    private evaluation: EvaluationJavaService,
     private moves: MovesService,
     private check: CheckBoardService) { }
 
   maxDepth: number = 3;
 
-  calculateComputerMove(board: string[][]): [number, number] | null {
+  calculateComputerMove(board: number[][]): [number, number] | null {
     const bestMove = this.minimax(board, this.maxDepth, true, -Infinity, Infinity);
     console.log("Best Move score: " + bestMove.score);
     console.log("Best Move: " + bestMove.move);
     return bestMove.move;
   }
 
-  minimax(board: string[][], depth: number, maximizingPlayer: boolean, alpha: number, beta: number): 
+  minimax(board: number[][], depth: number, maximizingPlayer: boolean, alpha: number, beta: number): 
   { score: number; move: [number, number] } {
   const result = this.check.checkGameStatus(board);
 
@@ -38,9 +103,9 @@ export class MinimaxService {
   const validMoves: [number, number][] = this.moves.generateMoves(board);
 
   for (const [row, col] of validMoves) {
-    board[row][col] = maximizingPlayer ? 'O' : 'X'; // Make a move
+    board[row][col] = maximizingPlayer ? 1 : 2; // Make a move
     const score = this.minimax(board, depth - 1, !maximizingPlayer, alpha, beta).score; 
-    board[row][col] = ''; // Undo the move
+    board[row][col] = 0; // Undo the move
 
     if (maximizingPlayer && score > bestScore) {
       bestScore = score;
--- Gomoku/src/app/services/moves.service.ts
+++ Gomoku/src/app/services/moves.service.ts
@@ -1,3 +1,65 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MovesService {
+
+//   constructor() { }
+
+//   generateMoves(board: string[][]): [number, number][] {
+//     const moveList: [number, number][] = [];
+  
+//     const boardSize = board.length;
+  
+//     // Look for cells that have at least one stone in an adjacent cell.
+//     for (let i = 0; i < boardSize; i++) {
+//       for (let j = 0; j < boardSize; j++) {
+//         if (board[i][j] !== '') continue;
+  
+//         if (i > 0) {
+//           if (j > 0) {
+//             if (board[i - 1][j - 1] !== '' || board[i][j - 1] !== '') {
+//               moveList.push([i, j]);
+//               continue;
+//             }
+//           }
+//           if (j < boardSize - 1) {
+//             if (board[i - 1][j + 1] !== '' || board[i][j + 1] !== '') {
+//               moveList.push([i, j]);
+//               continue;
+//             }
+//           }
+//           if (board[i - 1][j] !== '') {
+//             moveList.push([i, j]);
+//             continue;
+//           }
+//         }
+//         if (i < boardSize - 1) {
+//           if (j > 0) {
+//             if (board[i + 1][j - 1] !== '' || board[i][j - 1] !== '') {
+//               moveList.push([i, j]);
+//               continue;
+//             }
+//           }
+//           if (j < boardSize - 1) {
+//             if (board[i + 1][j + 1] !== '' || board[i][j + 1] !== '') {
+//               moveList.push([i, j]);
+//               continue;
+//             }
+//           }
+//           if (board[i + 1][j] !== '') {
+//             moveList.push([i, j]);
+//             continue;
+//           }
+//         }
+//       }
+//     }
+  
+//     return moveList;
+//   }
+  
+// }
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,7 +69,7 @@ export class MovesService {
 
   constructor() { }
 
-  generateMoves(board: string[][]): [number, number][] {
+  generateMoves(board: number[][]): [number, number][] {
     const moveList: [number, number][] = [];
   
     const boardSize = board.length;
@@ -15,40 +77,40 @@ export class MovesService {
     // Look for cells that have at least one stone in an adjacent cell.
     for (let i = 0; i < boardSize; i++) {
       for (let j = 0; j < boardSize; j++) {
-        if (board[i][j] !== '') continue;
+        if (board[i][j] !== 0) continue;
   
         if (i > 0) {
           if (j > 0) {
-            if (board[i - 1][j - 1] !== '' || board[i][j - 1] !== '') {
+            if (board[i - 1][j - 1] !== 0 || board[i][j - 1] !== 0) {
               moveList.push([i, j]);
               continue;
             }
           }
           if (j < boardSize - 1) {
-            if (board[i - 1][j + 1] !== '' || board[i][j + 1] !== '') {
+            if (board[i - 1][j + 1] !== 0 || board[i][j + 1] !== 0) {
               moveList.push([i, j]);
               continue;
             }
           }
-          if (board[i - 1][j] !== '') {
+          if (board[i - 1][j] !== 0) {
             moveList.push([i, j]);
             continue;
           }
         }
         if (i < boardSize - 1) {
           if (j > 0) {
-            if (board[i + 1][j - 1] !== '' || board[i][j - 1] !== '') {
+            if (board[i + 1][j - 1] !== 0 || board[i][j - 1] !== 0) {
               moveList.push([i, j]);
               continue;
             }
           }
           if (j < boardSize - 1) {
-            if (board[i + 1][j + 1] !== '' || board[i][j + 1] !== '') {
+            if (board[i + 1][j + 1] !== 0 || board[i][j + 1] !== 0) {
               moveList.push([i, j]);
               continue;
             }
           }
-          if (board[i + 1][j] !== '') {
+          if (board[i + 1][j] !== 0) {
             moveList.push([i, j]);
             continue;
           }
